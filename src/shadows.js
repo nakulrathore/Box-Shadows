783ebfe9439e7badaf9fbde113b57df6dbda9e77
Jaako <jaako.andes@boom.camp>
2019-10-02 17:28:23 +0800

    Added a shadow

--- src/shadows.js
+++ src/shadows.js
@@ -111,6 +111,11 @@ const shadows = [
 		"shadow": "0 1px 6px rgba(0,0,0,.1), 0 2px 24px rgba(0,0,0,.2)",
 		"border": "0"
 	},
+	{
+		"name": "Simple Material UI Button",
+		"shadow": "0 2px 5px 0 rgba(0, 0, 0, 0.225)",
+		"border": "0"
+	}
 ]
 
 
