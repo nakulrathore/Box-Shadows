fdb713bbabaee45847511b9fe3e061c2cf603872
Elijah Jeremiah L. Barba <elijahjeremiah.barba@bicol-u.edu.ph>
2019-10-04 09:31:07 +0800

    Update shadows.js

--- src/shadows.js
+++ src/shadows.js
@@ -1,108 +1,104 @@
 const shadows = [
-  {
-    name: "Google docs",
-    shadow: "0 0 0 0.75pt #d1d1d1, 0 0 3pt 0.75pt #ccc",
-    border: "0"
-  },
-  {
-    name: "Google Account Login",
-    shadow:
-      "0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2)",
-    border: "0"
-  },
-  {
-    name: "Google Notification",
-    shadow:
-      "0 16px 24px 2px rgba(0,0,0,0.14), 0 6px 30px 5px rgba(0,0,0,0.12), 0 8px 10px -5px rgba(0,0,0,0.2)",
-    border: "0"
-  },
-  {
-    name: "Google Cards",
-    shadow: "0 2px 2px 0 rgba(0,0,0,0.16), 0 0 0 1px rgba(0,0,0,0.08)",
-    border: "0"
-  },
-  {
-    name: "Udacity Chapters",
-    shadow: "0 0 20px 0 rgba(0, 0, 0, 0.12)",
-    border: "0"
-  },
-  {
-    name: "Udacity Buttons",
-    shadow: "12px 15px 20px rgba(0, 0, 0, 0.1)",
-    border: "0"
-  },
-  {
-    name: "Angular",
-    shadow: "0 6px 6px rgba(10,16,20,.15), 0 0 52px rgba(10,16,20,.12)",
-    border: "0"
-  },
-  {
-    name: "MDLBlog Cards",
-    shadow: "0 2px 5px rgba(0, 0, 0, 0.4)",
-    border: "0"
-  },
-  {
-    name: "Android.com Slider",
-    shadow: "-25px 50px 30px rgba(0,0,0,0.125)",
-    border: "0"
-  },
-  {
-    name: "I/O 2015",
-    shadow:
-      "0px 2px 5px 0px rgba(0,0,0,0.16), 0px 2px 5px 0px rgba(0,0,0,0.23)",
-    border: "0"
-  },
-  {
-    name: "medium post",
-    shadow: "0 1px 4px rgba(0,0,0,.04)",
-    border: "1px solid rgba(0,0,0,.09)"
-  },
-  {
-    name: "medium bottom",
-    shadow: "0 4px 2px -2px rgba(0,0,0,.05)",
-    border: "0"
-  },
-  {
-    name: "whatsapp web dropdown",
-    shadow: "0 2px 5px 0 rgba(0,0,0,0.26), 0 2px 10px 0 rgba(0,0,0,0.16)",
-    border: "0"
-  },
-  {
-    name: "testmysite.io",
-    shadow: "0 0 25px rgba(0,0,0,.05)",
-    border: "1px solid #f1f4ff"
-  },
-  {
-    name: "flipkart search",
-    shadow: "0 2px 4px 0 rgba(0, 0, 0, 0.23)",
-    border: "0"
-  },
-  {
-    name: "LinkedIn Post",
-    shadow: "0 0 0 1px rgba(0,0,0,.1), 0 2px 3px rgba(0,0,0,.2)",
-    border: "0"
-  },
-  {
-    name: "Digital Ocean",
-    shadow: "0 2px 4px rgba(3,27,78,.06)",
-    border: "1px solid #e5e8ed"
-  },
-  {
-    name: "Telegram login",
-    shadow:
-      "0 1px 1px rgba(97,127,152,.2), 1px 0 0 rgba(97,127,152,.1), -1px 0 0 rgba(97,127,152,.1)",
-    border: "0"
-  },
-  {
-    name: "Chat Button Widget Shadow",
-    shadow: "0 1px 6px rgba(0,0,0,.1), 0 2px 24px rgba(0,0,0,.2)",
-    border: "0"
-  },
-  {
-    name: "Nice Green Shadow",
-    shadow: "0px 15px 20px rgba(46, 229, 157, 0.4)",
-    border: "0"
-  }
-];
 
-export default shadows;
+	{
+		"name": "Google docs",
+		"shadow": "0 0 0 0.75pt #d1d1d1, 0 0 3pt 0.75pt #ccc",
+		"border": "0"
+	},
+	{
+		"name": "Google Account Login",
+		"shadow": "0 2px 2px 0 rgba(0,0,0,0.14), 0 3px 1px -2px rgba(0,0,0,0.12), 0 1px 5px 0 rgba(0,0,0,0.2)",
+		"border": "0"
+	},
+	{
+		"name": "Google Notification",
+		"shadow": "0 16px 24px 2px rgba(0,0,0,0.14), 0 6px 30px 5px rgba(0,0,0,0.12), 0 8px 10px -5px rgba(0,0,0,0.2)",
+		"border": "0"
+	},
+	{
+		"name": "Google Cards",
+		"shadow": "0 2px 2px 0 rgba(0,0,0,0.16), 0 0 0 1px rgba(0,0,0,0.08)",
+		"border": "0"
+	},
+	{
+		"name": "Udacity Chapters",
+		"shadow": "0 0 20px 0 rgba(0, 0, 0, 0.12)",
+		"border": "0"
+	},
+	{
+		"name": "Udacity Buttons",
+		"shadow": "12px 15px 20px rgba(0, 0, 0, 0.1)",
+		"border": "0"
+	},
+	{
+		"name": "Angular",
+		"shadow": "0 6px 6px rgba(10,16,20,.15), 0 0 52px rgba(10,16,20,.12)",
+		"border": "0"
+	},
+	{
+		"name": "MDLBlog Cards",
+		"shadow": "0 2px 5px rgba(0, 0, 0, 0.4)",
+		"border": "0"
+	},
+	{
+		"name": "Android.com Slider",
+		"shadow": "-25px 50px 30px rgba(0,0,0,0.125)",
+		"border": "0"
+	},
+	{
+		"name": "I/O 2015",
+		"shadow": "0px 2px 5px 0px rgba(0,0,0,0.16), 0px 2px 5px 0px rgba(0,0,0,0.23)",
+		"border": "0"
+	},
+	{
+
+		"name": "medium post",
+		"shadow": "0 1px 4px rgba(0,0,0,.04)",
+		"border": "1px solid rgba(0,0,0,.09)"
+	},
+	{
+		"name": "medium bottom",
+		"shadow": "0 4px 2px -2px rgba(0,0,0,.05)",
+		"border": "0"
+	},
+	{
+		"name": "whatsapp web dropdown",
+		"shadow": "0 2px 5px 0 rgba(0,0,0,0.26), 0 2px 10px 0 rgba(0,0,0,0.16)",
+		"border": "0"
+	},
+	{
+		"name": "testmysite.io",
+		"shadow": "0 0 25px rgba(0,0,0,.05)",
+		"border": "1px solid #f1f4ff"
+	},
+	{
+		"name": "flipkart search",
+		"shadow": "0 2px 4px 0 rgba(0, 0, 0, 0.23)",
+		"border": "0"
+	},
+	{
+		"name": "LinkedIn Post",
+		"shadow": "0 0 0 1px rgba(0,0,0,.1), 0 2px 3px rgba(0,0,0,.2)",
+		"border": "0"
+	},
+	{
+		"name": "Digital Ocean",
+		"shadow": "0 2px 4px rgba(3,27,78,.06)",
+		"border": "1px solid #e5e8ed"
+	},
+	{
+		"name": "Telegram login",
+		"shadow": "0 1px 1px rgba(97,127,152,.2), 1px 0 0 rgba(97,127,152,.1), -1px 0 0 rgba(97,127,152,.1)",
+		"border": "0"
+	},
+	{
+		"name": "Chat Button Widget",
+		"shadow": "0 1px 6px rgba(0,0,0,.1), 0 2px 24px rgba(0,0,0,.2)",
+		"border": "0"
+	},
+	{
+		"name": "Nice Green Shadow",
+		"shadow": "0px 15px 20px rgba(46, 229, 157, 0.4)",
+		"border": "0"
+	},
+]
