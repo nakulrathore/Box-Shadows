055eb6ec2b9a5945b3b8bb9882bb44133ff3de77
Sabiha Shaik <sabihaabc1@gmail.com>
2019-10-10 22:28:03 +0400

    Create GithubRibbon.js

---
+++ src/components/GithubRibbon.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+class Banner extends Component{
+  render(){
+     return (
+     
+      <span id="forkongithub">
+        <a href="https://github.com/nakulrathore/Box-Shadows">
+          Fork me on GitHub
+        </a>
+      </span>
+    );
+  }
+}
+export default Banner;
