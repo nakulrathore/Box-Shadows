f3b51676b41c70e84f6b39e069c8d0b8bfcf04e3
itsvvishnu <vishnushanmukan@gmail.com>
2019-10-15 14:58:52 +0530

    vishnu v

--- src/shadows.js
+++ src/shadows.js
@@ -255,10 +255,16 @@ const shadows = [
 		"name": "Codepen Material design",
 		"shadow": " 0 19px 38px rgba(0,0,0,0.30), 0 15px 12px rgba(0,0,0,0.22)",
 		"border": "0"
-  },
+	},
+	{
 		"name": "Quicken Loans Shadow",
 		"shadow": "0 20px 30px 0 #e5e5e5",
 		"border": "2px solid #2081bf"
 	},
+	{
+		"name":"Fury",
+		"shadow":"0px 0px 10px 4px #e0e0e0",
+		"border":"1px solid #daf1ff"
+	}
 ]
 export default shadows;
