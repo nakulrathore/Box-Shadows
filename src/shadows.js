ec500471c290cc1fb17e45e8aa5bfdbe82e00f0f
jpacsai <judit.pacsai@gmail.com>
2020-10-03 20:09:29 +0200

    feat: add Starbucks boxshadow

--- src/shadows.js
+++ src/shadows.js
@@ -389,5 +389,10 @@ const shadows = [
     shadow: "0 0 0 1px rgba(0,0,0,.1), 0 2px 4px 1px rgba(0,0,0,.18)",
     border: "1px solid transparent"
   },
+  {
+    name: "Starbucks",
+    shadow: "0 11px 15px 0 rgba(0, 0, 0, 0.1), 0 9px 46px 8px rgba(0, 0, 0, 0.06), 0 24px 38px 3px rgba(0, 0, 0, 0.07)",
+    border: "1.5px solid rgba(0, 0, 0, .12)"
+  }
 ];
 export default shadows;
