dc10134cef47bf2cafab523c4daa0c85d6b9e40f
Shaileshsachan <46265361+Shaileshsachan@users.noreply.github.com>
2021-05-10 18:51:13 +0530

    Added Disney +
    Sign Out Shadow for dropDown

--- src/shadows.js
+++ src/shadows.js
@@ -433,6 +433,11 @@ const shadows = [
     name: "Starbucks",
     shadow: "0 11px 15px 0 rgba(0, 0, 0, 0.1), 0 9px 46px 8px rgba(0, 0, 0, 0.06), 0 24px 38px 3px rgba(0, 0, 0, 0.07)",
     border: "1.5px solid rgba(0, 0, 0, .12)"
-  }
+  },
+  {
+    name: "Disney +",
+    shadow: "rgb(0 0 0 / 50%) 0px 0px 18px 0px)",
+    border: "1px solid rgba(151, 151, 151, 0.34)"
+  },
 ];
 export default shadows;
